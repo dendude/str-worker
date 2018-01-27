5117fc550094b57a4890bd457eac0a1db09d2941
Denis Kravtsov <denis.kravtsov.1986@mail.ru>
2018-01-27 22:54:50 +0300

    docker config

--- .docker/run-httpd.sh
+++ .docker/run-httpd.sh
@@ -5,6 +5,11 @@ mkdir -p ${LOG_DIR}
 touch ${LOG_DIR}/error.log
 chown ${USER_APACHE}:${USER_APACHE} ${LOG_DIR}/error.log
 
+# Creating config
+mkdir -p ${RABBITMQ_CONFIG_DIR}
+touch ${RABBITMQ_CONFIG_FILE}
+chown ${USER_APACHE}:${USER_APACHE} ${RABBITMQ_CONFIG_FILE}
+
 # Update main httpd.conf
 sed -i \
     -e 's~AllowOverride \(.*\)$~AllowOverride All~g' \
@@ -17,9 +22,17 @@ sed -i \
     /etc/php.ini
 
 # Update .htaccess
-cat ${PRROOT}/docker/htaccess.prepare \
-    | sed -e 's~<%=log_path%>~'${LOG_DIR}'~' \
+cat ${PRROOT}/docker/prepare.htaccess \
+    | sed -e 's~<%=log_path %>~'${LOG_DIR}'~' \
     > ${PRROOT}/web/.htaccess
+
+# Update rabbitmq config
+cat ${PRROOT}/docker/prepare.rabbitmq \
+    | sed -e 's~<%=host %>~'${RABBITMQ_HOST}'~' \
+    | sed -e 's~<%=port %>~'${RABBITMQ_PORT}'~' \
+    | sed -e 's~<%=user %>~'${RABBITMQ_USER}'~' \
+    | sed -e 's~<%=pass %>~'${RABBITMQ_PASS}'~' \
+    > ${RABBITMQ_CONFIG_FILE}
 
 # Make sure we're not confused by old, incompletely-shutdown httpd
 # context after restarting the container.  httpd won't start correctly
